895a817a518131d50fa70967cb8c7e2a3111695b
Ekaterina <rfvb.foto@mail.ru>
2024-01-27 10:09:22 +0200

    Добавьте файлы проекта.

---
+++ Игра 21/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Игра_21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здраствуйте, сколько у вас карт на руках?");
+            int cards = int.Parse(Console.ReadLine());
+            int summ = 0;
+           
+
+            for (int i = 0; i<cards; i++ )
+            {
+                Console.WriteLine("Ввидите номинал карты");
+                string denomination = Console.ReadLine();
+
+    
+                switch (denomination)
+                {
+                    case "J":
+                    case "K":
+                    case "Q":
+                    case "T": 
+                        summ = summ + 10;
+                        break;
+                    default:
+                       int number = int.Parse(denomination);
+                        summ = summ + number;
+                        break;
+
+                }
+
+            }
+            Console.WriteLine("Сумма карт = " + summ) ;
+            Console.ReadKey();
+          
+        }
+    }
+}
